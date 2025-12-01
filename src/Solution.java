74e317bbb3a72d9b76adf33951b9348e6d03cd67
jalaverdiyeva <93456693+jalaverdiyeva@users.noreply.github.com>
2025-12-01 16:24:06 +0400

    Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+
 
 public class Solution {
+    //metod yaradılır -> nəticə olaraq true və ya false dəyərini döndərəcək
+    public boolean containsDuplicate(int[] nums) { //given an integer array ->> nums
+        java.util.Arrays.sort(nums);//sıralama bizə eyni ədədlər qonşu vəziyyətə gəlir deyə kömək edir.
+        for(int i=0;i<nums.length-1;i++){//loop-dur, massivdə bütün elementləri gəzəcək
+            if (nums[i] == nums[i+1]){
+                return true; // return true if any value appears at least twice in the array
+            }
+        }
+        return false;// and return false if every element is distinct.
+    }
 }
